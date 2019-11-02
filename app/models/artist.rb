7caeba20ba11af50f4bfd35130e9689cd5e5b2d3
Qing <chingwang.qw@gmail.com>
2019-11-02 18:13:45 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,70 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+ # Returns an array all the paintings by an artist
+  def paintings
+    Painting.all.select {|p| p.artist == self}
+  end
+
+  def galleries
+    paintings.map {|p| p.gallery}
+  end
+
+  #Return an array of all cities that an artist has paintings in
+  def cities
+    galleries.map {|g| g.city}
+  end
+
+ 
+  #Returns an integer that is the total years of experience of all artists
+  def self.total_experience
+    (self.all.map {|a| a.years_experience}).sum
+  end
+
+  #Returns an instance of the artist with the highest amount of paintings per year of experience.
+  def self.most_prolific
+    total_experience_array = self.all.map {|a| a.years_experience}.sort
+    the_most = total_experience_array.pop()
+  end
+
+  #Given the arguments of title, price and gallery, creates a new painting belonging to 
+  ##title, price, artist, gallery
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
+
+
+=begin
+Artist#paintings
+
+
+Artist#galleries
+
+Returns an array of all the galleries that an artist has paintings in
+Artist#cities
+
+Return an array of all cities that an artist has paintings in
+Artist.total_experience
+
+Returns an integer that is the total years of experience of all artists
+Artist.most_prolific
+
+Returns an instance of the artist with the highest amount of paintings per year of experience.
+Artist#create_painting
+
+Given the arguments of title, price and gallery, creates a new painting belonging to that artist
+  
+=end 
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,25 @@
 require_relative '../config/environment.rb'
 
+######### Artist Testing ############
+a1 = Artist.new("Qing", 5)
+a2 = Artist.new("Tom", 6)
+a3 = Artist.new("Alex", 10)
+a4 = Artist.new("Seteve", 30)
+
+######### Gallery Testing ############
+#(name, city)
+g1 = Gallery.new("London Gallery", "London")
+g2 = Gallery.new("New York Gallery", "New York")
+g3 = Gallery.new("Shanghai Gallery", "Shanghai")
 
 
+######### Painting Testing ############
+#title, price, artist, gallery
+Painting.new("Sunflower", 10000, a1, g2)
+Painting.new("Fruits", 600, a1, g1)
+Painting.new("Master Piece", 333, a2, g2)
+Painting.new("Starts", 9000, a3, g3) 
+Painting.new("Blue Sky", 40000, a4, g1)
 
 binding.pry
 
