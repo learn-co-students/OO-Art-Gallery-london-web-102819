06b8c79395ed59a145053f99804e9cfd02e0f5c2
siapankina <siapankina@gmail.com>
2019-11-03 19:12:30 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,50 @@
 class Artist
-
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def paintings
+    Painting.all.select { |p|
+      p.artist == self
+    }
+  end
+
+  def galleries
+    paintings.map { |p|
+      p.gallery
+    }.uniq
+  end
+
+  def cities
+    galleries.map { |g|
+      g.city
+    }
+  end
+
+  def self.total_experience
+    total_years = 0
+    Artist.all.each { |a|
+      total_years += a.years_experience.to_i
+    }
+    total_years
+  end
+
+  def self.most_prolific
+    Artist.all.max_by { |a|
+      a.paintings.length / a.years_experience
+    }
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+  def self.all
+    @@all
+  end
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,38 @@
 class Gallery
-
   attr_reader :name, :city
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select { |p|
+      p.gallery == self
+    }
+  end
+
+  def artists
+    paintings.map { |p|
+      p.artist
+    }.uniq
+  end
+
+  def artist_names
+    artists.map { |a|
+      a.name
+    }
+  end
+
+  def most_expensive_painting
+    paintings.max_by { |p|
+      p.price
+    }
+  end
 end
