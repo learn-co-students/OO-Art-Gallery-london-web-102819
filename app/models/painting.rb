09f4e97fd51a30ab5ce5d1c9b2464f526663efac
PianoFF <Fangfei1star@gmail.com>
2019-11-04 17:25:17 +0000

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,22 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :gallery, :artist
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, gallery, artist)
+    #before you start, read the readme and figureout which model should be the middle man.
+    #an artist has many paintings
+    #a gallery has many paintings
+    #by giving the painties, add the gallery and artist to be initialized 
     @title = title
     @price = price
+    @gallery = gallery
+    @artist = artist
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 end
